cb7771d08c36866813b5f5503ec9c7eeb35c9a96
ppking <ppking2897@gmail.com>
2016-11-24 16:45:37 +0800

    FireBase 推播

---
+++ app/src/main/java/com/example/user/android_class_15/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.user.android_class_15;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity {
+    private FirebaseDatabase database;
+    private DatabaseReference myRef;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference("message");
+
+        init();
+    }
+    public void SendData(View v){
+        // Write a message to the database
+        myRef.setValue("Hello, World!"+(int)(Math.random()*49+1));
+    }
+    // Read from the database
+    public void init() {
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.v("ppking", "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("ppking", "Failed to read value.", error.toException());
+            }
+        });
+    }
+}
